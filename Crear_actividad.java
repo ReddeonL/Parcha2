b8b4da245414d923096115c67fda44ae335ff31f
MarthaMC <93721382+MarthaMC@users.noreply.github.com>
2021-11-21 18:14:50 -0500

    Add files via upload

---
+++ Crear_actividad.java
@@ -0,0 +1,14 @@
+package com.example.parcha2app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Crear_actividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_actividad);
+    }
+}
